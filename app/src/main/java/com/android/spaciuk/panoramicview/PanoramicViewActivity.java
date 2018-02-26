c9d7e5380a1ca110834215b5c670cbcf6f67523f
spaciuk <sebapaciuk@gmail.com>
2018-02-25 21:03:49 -0300

    Added mqtt integration
    Now it connects to a local mqtt server, suscribe to a topic and receive
    messages with the image name to show.

--- app/src/main/java/com/android/spaciuk/panoramicview/PanoramicViewActivity.java
+++ app/src/main/java/com/android/spaciuk/panoramicview/PanoramicViewActivity.java
@@ -6,7 +6,6 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.util.Pair;
 
@@ -18,20 +17,36 @@ import com.google.vr.sdk.widgets.pano.VrPanoramaView;
 import java.io.IOException;
 import java.io.InputStream;
 
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
 public class PanoramicViewActivity extends Activity {
 
     private static final String TAG = PanoramicViewActivity.class.getSimpleName();
-    private static final int IMAGES_AMOUNT = 4;
-    private static final int DELAY = 5000; //Expressed in milliseconds
     /** Actual panorama widget. **/
     private VrPanoramaView panoWidgetView;
     /** Tracks the file to be loaded across the lifetime of this app. **/
-    private Uri fileUri;
+    private Uri fileUri = null;
     /** Configuration information for the panorama. **/
     private Options panoOptions = new Options();
     private ImageLoaderTask backgroundImageLoaderTask;
-    private Handler handler;
-    private int imageIndex = 0;
+    private String nextImage = null;
+    /* MQTT variables */
+    private MqttAndroidClient mqttAndroidClient;
+    private String subscriptionTopic = null;
+    private String serverUri = null;
+    private String clientId = null;
+    private String first_message_part = null;
+    private String mqtt_user = null;
+    private String mqtt_password = null;
     /**
      * Called when the app is launched via the app icon or an intent using the adb command above. This
      * initializes the app and loads the image to render.
@@ -42,41 +57,7 @@ public class PanoramicViewActivity extends Activity {
 
         initializeActivity();
 
-        handler = new Handler();
-        handler.postDelayed(new Runnable(){
-            public void run(){
-                changeImage();
-                handler.postDelayed(this, DELAY);
-            }
-        }, DELAY);
-    }
-
-    protected void initializeActivity(){
-        setContentView(R.layout.activity_panoramic_view);
-        panoWidgetView = (VrPanoramaView) findViewById(R.id.pano_view);
-
-        panoWidgetView.setInfoButtonEnabled(false);
-        panoWidgetView.setFullscreenButtonEnabled(false);
-        panoWidgetView.setStereoModeButtonEnabled(false);
-        panoWidgetView.setDisplayMode(VrWidgetView.DisplayMode.FULLSCREEN_STEREO);
-
-        panoWidgetView.setEventListener(new VrPanoramaEventListener());
-        changeImage();
-    }
-
-    protected void changeImage(){
-        this.imageIndex = this.imageIndex % IMAGES_AMOUNT + 1;
-        Log.i(TAG, "New Image. imageIndex = " + imageIndex);
-        fileUri = null;
-        panoOptions.inputType = Options.TYPE_STEREO_OVER_UNDER;
-        // Load the bitmap in a background thread to avoid blocking the UI thread. This operation can
-        // take 100s of milliseconds.
-        if (backgroundImageLoaderTask != null) {
-            // Cancel any task from a previous intent sent to this activity.
-            backgroundImageLoaderTask.cancel(true);
-        }
-        backgroundImageLoaderTask = new ImageLoaderTask();
-        backgroundImageLoaderTask.execute(Pair.create(fileUri, panoOptions));
+        initializeMQTT();
     }
 
     @Override
@@ -104,6 +85,131 @@ public class PanoramicViewActivity extends Activity {
         super.onDestroy();
     }
 
+    protected void initializeActivity(){
+        setContentView(R.layout.activity_panoramic_view);
+        panoOptions.inputType = Options.TYPE_STEREO_OVER_UNDER;
+
+        subscriptionTopic = getString(R.string.topic_name);
+        serverUri = getString(R.string.mqtt_server_url);
+        clientId = getString(R.string.client_id);
+        first_message_part = getString(R.string.first_message_part);
+        mqtt_user = getString(R.string.mqtt_user);
+        mqtt_password = getString(R.string.mqtt_password);
+
+        panoWidgetView = (VrPanoramaView) findViewById(R.id.pano_view);
+
+        panoWidgetView.setInfoButtonEnabled(false);
+        panoWidgetView.setFullscreenButtonEnabled(false);
+        panoWidgetView.setStereoModeButtonEnabled(false);
+        panoWidgetView.setDisplayMode(VrWidgetView.DisplayMode.FULLSCREEN_STEREO);
+
+        panoWidgetView.setEventListener(new VrPanoramaEventListener());
+    }
+
+    protected void changeImage(String imageName){
+        nextImage = imageName;
+        Log.i(TAG, "New Image: " + nextImage);
+        // Load the bitmap in a background thread to avoid blocking the UI thread. This operation can
+        // take 100s of milliseconds.
+        if (backgroundImageLoaderTask != null) {
+            // Cancel any task from a previous intent sent to this activity.
+            backgroundImageLoaderTask.cancel(true);
+        }
+        backgroundImageLoaderTask = new ImageLoaderTask();
+        backgroundImageLoaderTask.execute(Pair.create(fileUri, panoOptions));
+    }
+
+    protected void initializeMQTT(){
+
+        mqttAndroidClient = new MqttAndroidClient(getApplicationContext(),serverUri,clientId);
+        mqttAndroidClient.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+                if (reconnect) {
+                    Log.i(TAG,"Reconnected to : " + serverURI);
+                    // Because Clean Session is true, we need to re-subscribe
+                    subscribeToTopic();
+                } else {
+                    Log.i(TAG,"Connected to: " + serverURI);
+                }
+            }
+            @Override
+            public void connectionLost(Throwable cause) {
+                Log.i(TAG,"The Connection was lost.");
+            }
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                Log.i(TAG,"Incoming message: " + new String(message.getPayload()));
+            }
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
+
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        mqttConnectOptions.setAutomaticReconnect(true);
+        mqttConnectOptions.setCleanSession(false);
+        mqttConnectOptions.setUserName(mqtt_user);
+        mqttConnectOptions.setPassword(mqtt_password.toCharArray());
+
+        try {
+            Log.i(TAG,"Connecting to " + serverUri);
+            mqttAndroidClient.connect(mqttConnectOptions, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    DisconnectedBufferOptions disconnectedBufferOptions = new DisconnectedBufferOptions();
+                    disconnectedBufferOptions.setBufferEnabled(true);
+                    disconnectedBufferOptions.setBufferSize(100);
+                    disconnectedBufferOptions.setPersistBuffer(false);
+                    disconnectedBufferOptions.setDeleteOldestMessages(false);
+                    mqttAndroidClient.setBufferOpts(disconnectedBufferOptions);
+                    subscribeToTopic();
+                }
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.e(TAG,"Failed to connect to: " + serverUri);
+                    Log.e(TAG, exception.getMessage());
+                }
+            });
+        } catch (MqttException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    protected void subscribeToTopic(){
+        try {
+            mqttAndroidClient.subscribe(subscriptionTopic, 0, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.i(TAG,"Subscribed!");
+                }
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.i(TAG,"Failed to subscribe");
+                }
+            });
+
+            // THIS DOES NOT WORK!
+            mqttAndroidClient.subscribe(subscriptionTopic, 0, new IMqttMessageListener() {
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    // message Arrived!
+                    String message_payload = new String(message.getPayload());
+                    Log.i(TAG,"Message: " + topic + " : " + message_payload);
+                    if(message_payload.contains(first_message_part)){
+                        String[] split = message_payload.split(first_message_part);
+                        changeImage(split[0]); // Look ! It must pass the name of the file only.
+                    }
+                }
+            });
+
+        } catch (MqttException ex){
+            Log.e(TAG,"Exception whilst subscribing");
+            ex.printStackTrace();
+        }
+    }
+
     /**
      * Helper class to manage threading.
      */
@@ -113,14 +219,10 @@ public class PanoramicViewActivity extends Activity {
          */
         @Override
         protected Boolean doInBackground(Pair<Uri, Options>... fileInformation) {
-            Options panoOptions = null;  // It's safe to use null VrPanoramaView.Options.
             InputStream istr = null;
-            String imageName = "image_" + imageIndex + ".jpg";
             AssetManager assetManager = getAssets();
             try {
-                istr = assetManager.open(imageName);
-                panoOptions = new Options();
-                panoOptions.inputType = Options.TYPE_STEREO_OVER_UNDER;
+                istr = assetManager.open(nextImage);
             } catch (IOException e) {
                 Log.e(TAG, "Could not decode default bitmap: " + e);
                 return false;
